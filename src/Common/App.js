14af8d121e105b071d50017f92d5c0d74869b033
Nikolai Basik <Nikolai_Basik@epam.com>
2018-03-09 18:01:28 +0300

    added configuration file and implemented fixes for data handling

--- src/Common/App.js
+++ src/Common/App.js
@@ -1,123 +1,150 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, {Component} from "react";
 import "./App.css";
+import {
+	MAX_WATER_LEVEL,
+	MIN_TEMPERATURE_LEVEL,
+	MAX_TEMPERATURE_LEVEL,
+	MIN_PRESSURE_LEVEL,
+	MAX_PRESSURE_LEVEL,
+	MIN_VOLUME_LEVEL,
+	MAX_VOLUME_LEVEL,
+	TEMPERATURE_MEASURE,
+	PRESSURE_MEASURE,
+	VOLUME_MEASURE,
+	START_COMMAND,
+	STOP_COMMAND,
+	DRAIN_COMMAND,
+	WS_SERVER
+} from "../helpers/consts";
 import Gauge from "../Charts/Gauge";
 import SolidGauge from "../Charts/SolidGauge";
 
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      socket: {},
-      pumpData: ""
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			socket: "",
+			pumpData: {
+				temperature: 0,
+				pressure: 0,
+				volume: 0,
+				height: 0
+			}
+		};
+	}
 
-  stopProcess() {
-    let message = {
-      type: "stop"
-    };
-    this.state.socket.send(JSON.stringify(message));
-  }
+	startProcess() {
+		const socket = new WebSocket(WS_SERVER);
 
-  startDrain(height) {
-    let str = JSON.stringify({
-      type: "drain",
-      height: height
-    });
-    this.state.socket.send(str);
-  }
+		socket.onopen = () => {
+			console.log("WebSocket channel is opened");
+			const data = this.state.pumpData;
+			let message = {
+				type: START_COMMAND,
+				height: data ? data.height : 0
+			};
+			this.setState({
+				socket: socket
+			}, () => socket.send(JSON.stringify(message)));
+		};
 
-  startProcess() {
-    const socket = new WebSocket("ws://localhost:3000");
+		socket.onmessage = event => {
+			const dataObj = event.data ? JSON.parse(event.data) : {};
+			this.setState({
+				pumpData: dataObj
+			}, () => {
+				if (dataObj.height > MAX_WATER_LEVEL) {
+					socket.send(JSON.stringify({type: STOP_COMMAND}));
+				}
 
-    socket.onopen = () => {
-      const data = this.state.pumpData;
-      console.log("WebSocket channel is opened");
+				if (dataObj.height < 0) {
+					socket.send(JSON.stringify({type: STOP_COMMAND}));
+					socket.close();
+				}
+			});
+		};
 
-      let message = {
-        type: "start",
-        height: data ? data.height : 0
-      };
-      socket.send(JSON.stringify(message));
-      this.setState({
-        socket: socket
-      });
-    };
+		socket.onclose = event => {
+			console.log("Connection was closed");
+		};
 
-    socket.onmessage = event => {
-      const dataObj = event.data ? JSON.parse(event.data) : {};
-      this.setState({
-        pumpData: dataObj
-      });
+		socket.onerror = event => {
+			console.log("Error occurred " + event);
+			socket.close();
+		};
+	}
 
-      if (dataObj.height > 10) {
-        socket.send(JSON.stringify({ type: "stop" }));
-      }
+	startDrain() {
+		const data = this.state.pumpData;
+		let message = JSON.stringify({
+			type: DRAIN_COMMAND,
+			height: data.height ? data.height : 0
+		});
+		if (this.state.socket) {
+			this.state.socket.send(message);
+		}
+	}
 
-      if (dataObj.height < 0) {
-        socket.send(JSON.stringify({ type: "stop" }));
-      }
-    };
+	stopProcess() {
+		let message = {
+			type: STOP_COMMAND
+		};
+		if (this.state.socket) {
+			this.state.socket.send(JSON.stringify(message));
+		}
+	}
 
-    socket.onclose = event => {
-      console.log("Connection was closed");
-    };
 
-    socket.onerror = event => {
-      console.log("Error occurred " + event);
-      socket.close();
-    };
-  }
+	render() {
+		const pumpData = this.state.pumpData;
 
-  render() {
-    const pumpData = this.state.pumpData;
+		return (
+			<div className="App">
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Futuristic Pump</h1>
-        </header>
+				<button onClick={() => this.startProcess()}>START</button>
+				<button onClick={() => this.stopProcess()}>STOP</button>
+				<button onClick={() => this.startDrain()}>DRAIN</button>
 
-        <button onClick={() => this.startProcess()}>START</button>
-        <button onClick={() => this.stopProcess()}>STOP</button>
-        <button onClick={() => this.startDrain(pumpData.height)}>DRAIN</button>
+				<div>
+					<h2>Temperature</h2>
+					<span>{parseInt(pumpData.temperature) || 0} </span>
 
-        <div>
-          <h2>Temperature</h2>
-          <span>{pumpData.temperature || 0} </span>
-          <h2>Pressure</h2>
-          <span>{pumpData.pressure || 0}</span>
-          <h2>Volume</h2>
-          <span>{pumpData.volume || 0}</span>
-          <h2>Water Level (Max level: 10 m)</h2>
-          <span>{pumpData.height > 10 ? 10 : pumpData.height || 0}</span>
-        </div>
+					<h2>Pressure</h2>
+					<span>{pumpData.pressure || 0}</span>
 
-        <Gauge
-          data={pumpData.temperature}
-          text="Temperature"
-          interval={{ min: 0, max: 20 }}
-          measure={`${String.fromCharCode(248)}C`}
-        />
+					<h2>Volume</h2>
+					<span>{pumpData.volume || 0}</span>
 
-        <Gauge
-          data={pumpData.pressure}
-          text="Pressure"
-          interval={{ min: 0, max: 120000 }}
-          measure="Pa"
-        />
+					<h2>Water Level (Max level: {MAX_WATER_LEVEL} m)</h2>
+					<span>{pumpData.height > MAX_WATER_LEVEL
+						? MAX_WATER_LEVEL
+						: pumpData.height > 0 ? pumpData.height : 0}</span>
+				</div>
 
-        <SolidGauge
-          data={pumpData.volume}
-          text="Volume"
-          interval={{ min: 0, max: 300 }}
-          measure="m3"
-        />
-      </div>
-    );
-  }
+				<Gauge
+					data={parseInt(pumpData.temperature)}
+					text="Temperature"
+					interval={{min: MIN_TEMPERATURE_LEVEL, max: MAX_TEMPERATURE_LEVEL}}
+					measure={TEMPERATURE_MEASURE}
+				/>
+
+				<Gauge
+					data={pumpData.pressure}
+					text="Pressure"
+					interval={{min: MIN_PRESSURE_LEVEL, max: MAX_PRESSURE_LEVEL}}
+					measure={PRESSURE_MEASURE}
+				/>
+
+				<SolidGauge
+					data={pumpData.volume}
+					text="Volume"
+					interval={{min: MIN_VOLUME_LEVEL, max: MAX_VOLUME_LEVEL}}
+					measure={VOLUME_MEASURE}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/helpers/consts.js
@@ -0,0 +1,21 @@
+import config from "../config.json";
+
+export const WS_SERVER = config.wsServer;
+
+export const START_COMMAND = "start";
+export const STOP_COMMAND = "stop";
+export const DRAIN_COMMAND = "drain";
+
+export const MAX_WATER_LEVEL = config.maxWaterLevel;
+
+export const MIN_TEMPERATURE_LEVEL = config.temperatureInterval.min;
+export const MAX_TEMPERATURE_LEVEL = config.temperatureInterval.max;
+export const TEMPERATURE_MEASURE = `${String.fromCharCode(248)}C`;
+
+export const MIN_PRESSURE_LEVEL = config.pressureInterval.min;
+export const MAX_PRESSURE_LEVEL = config.pressureInterval.max;
+export const PRESSURE_MEASURE = 'Pa';
+
+export const MIN_VOLUME_LEVEL = config.volumeInterval.min;
+export const MAX_VOLUME_LEVEL = config.volumeInterval.max;
+export const VOLUME_MEASURE = 'm3';
